0f436fd2a8205d8ffe8cee242f50a8bacac5bfa8
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-23 16:57:54 -0300

    Fix: remove a component SECTION

--- src/components/Section/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from "react";
-import "./index.css" 
-
-export default () => {
-    return (
-        <section className="section"></section>
-    );
-};
