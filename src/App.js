8e268cd984529c020a7ec44da07d053400535644
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-23 14:35:19 -0300

    FIx: remove all files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import '../src/global/Styles/Style.css';
-
-import Header from './screen/header/Header';
-import Home from './screen/home/Home'
-import About from './screen/about/About'
-
 function App() {
   return (
-    <body>
-        <Header></Header>
-      <main class="main">
-        <Home></Home>
-        <About></About>
-      </main>
-      
-    </body>
+    <></>
   );
 }
 
