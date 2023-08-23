025d4bb3f35d571c98ccf9135b84ef690f62f713
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-23 19:12:19 -0300

    Fix: create a mock HeaderItems

---
+++ src/mock/HomeItem/index.js
@@ -0,0 +1,32 @@
+export const NavLinks = [
+    {
+        id: 1,
+        classNav: 'nav__link active-link',
+        ref: '#home',
+        text: 'Home'
+    },
+    {
+        id: 2,
+        classNav: 'nav__link',
+        ref: '#about',
+        text: 'Sobre Mim'
+    },
+    {
+        id: 3,
+        classNav: 'nav__link',
+        ref: '#projects',
+        text: 'Projetos'
+    },
+    {
+        id: 4,
+        classNav: 'nav__link',
+        ref: '#experience',
+        text: 'Experiências'
+    },
+    {
+        id: 5,
+        classNav: 'nav__link',
+        ref: '#contact',
+        text: 'Contato'
+    }
+] 
