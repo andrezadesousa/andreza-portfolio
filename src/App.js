6b1386154966b1cba9b24fcba0db708e9b700973
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-23 19:34:38 -0300

    Fix: delete files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css'
 
-import Header from "./pages/Header";
-import Home from "./pages/Home"
 
 function App() {
   return (
     <body>
-      <Header />
-      <main className='main'>
-        <Home />
-        <Home />
-      </main>
     </body>
   );
 }
