37bdfc6f6b5b78ae4674b1e00e17f9a076387c79
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-23 16:41:15 -0300

    Fix: create a new component SECTION

---
+++ src/components/Section/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./index.css" 
+
+export default () => {
+    return (
+        <section className="section"></section>
+    );
+};
