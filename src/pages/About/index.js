587ba648f4d58778a11ea8d9cab223c1b0b13a37
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-29 17:31:48 -0300

    Fix: create About page

---
+++ src/pages/About/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./index.css";
+
+import Section from "../../components/Section"
+import ContainerGrid from "../../components/ContainerGrid"
+
+export default () => {
+  return (
+    <Section id="about">
+      <ContainerGrid />
+    </Section>
+  );
+};
