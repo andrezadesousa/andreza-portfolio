4847c991ad720ef2a50186b004d7bae0bf0b6177
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-31 20:08:21 -0300

    Fix: add news images

---
+++ src/mocks/MockProjects.js
@@ -0,0 +1,74 @@
+export const Projects = [
+  {
+    id: 1,
+    image: require("../images/project-1.png"),
+    subtitle: "Website",
+    title: "Coffe & Tea",
+    description:
+      "Landing Page de uma cafeteria, responsivo e desenvolvido em mobile-first, utilizando a tecnologia: ReactJS",
+    link: "https://github.com/andrezadesousa/LP-Tea-And-Bread-ReactJs",
+  },
+  {
+    id: 2,
+    image: require("../images/project-2.png"),
+    subtitle: "Website",
+    title: "Netflix Clone",
+    description:
+      "Netflix clone, é um projeto desenvolvido em live pela B7Web curso. Trata-se da página de catálogo da NetFlix.",
+    link: "https://github.com/andrezadesousa/clone-netflix-react",
+  },
+  {
+    id: 3,
+    image: require("../images/project-3.png"),
+    subtitle: "Website",
+    title: "Easybank",
+    description:
+      "Landing Page de um banco digital e responsivo.",
+    link: "https://github.com/andrezadesousa/Easybank-Login-Responsive-ReactJs",
+  },
+  {
+    id: 4,
+    image: require("../images/project-4.jpeg"),
+    subtitle: "Website",
+    title: "Website",
+    description:
+      "Project that you carry out in the design and structure of the layout, showing the design at the client's request",
+    link: "https://github.com/",
+  },
+  {
+    id: 5,
+    image: require("../images/project-5.png"),
+    subtitle: "Website",
+    title: "Jazz School",
+    description:
+      "Jazz School trata-se de uma landing page, simples, com uma estética mais animada e colorida. Além, de ser desenvolvida no conceito mobile first.",
+    link: "https://github.com/andrezadesousa/jazz-school-lp",
+  },
+  {
+    id: 6,
+    image: require("../images/project-6.png"),
+    subtitle: "Website",
+    title: "Portfólio Rian Lot",
+    description:
+      "Projeto de um portfólio do Rian Lot, esenvolvido com o conceito de Mobile First, depois para desktop.",
+    link: "",
+  },
+  {
+    id: 7,
+    image: require("../images/project-7.png"),
+    subtitle: "Website",
+    title: "LetMeAsk | Sala virtual Q&A",
+    description:
+      "LetmeAsk é voltado para os amantes da leitura poderem criar suas salas de Q&A com seu público, assim, indicarem livros, sugestões de leitura e dúvidas sobre um livro ou seu escritor favorito. Tudo de forma organizada, democrática e interativa.",
+    link: "https://github.com/andrezadesousa/let-me-ask",
+  },
+  {
+    id: 8,
+    image: require("../images/project-8.png"),
+    subtitle: "Website",
+    title: "React Design System",
+    description:
+      "Projeto desenvolvido durante o bootcamp Ignite Lab da RocketSeat",
+    link: "https://github.com/andrezadesousa/react-design-system",
+  },
+];
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -5,6 +5,8 @@ import "./index.css";
 import SecondTitle from "../../components/Titles/SecondTitle";
 import ArticleProjects from "../../components/ArticleProjects"
 
+import { Projects } from "../../mocks/MockProjects";
+
 import Project1 from "../../images/project-1.png"
 
 export default () => {
@@ -13,27 +15,20 @@ export default () => {
       <SecondTitle title="Projetos." />
 
       <div className="projects__container container grid">
-        <ArticleProjects
-          image={Project1}
-          subtitle="Website"
-          title="Restaurant Website"
-          description="Project that you carry out in the design
-          and structure of the layout, showing the
-          design at the client's request."
-          link="https://github.com/"
-          icon="ri-github-line" 
-        />
-        
-        <ArticleProjects
-          image={Project1}
-          subtitle="Website"
-          title="Restaurant Website"
-          description="Project that you carry out in the design
-          and structure of the layout, showing the
-          design at the client's request."
-          link="https://github.com/"
-          icon="ri-github-line" 
-        />
+        {Projects.map((projectsLinks) => {
+          return(
+            <ArticleProjects
+            key={projectsLinks.id}
+            image={projectsLinks.image}
+            subtitle={projectsLinks.subtitle}
+            title={projectsLinks.title}
+            description={projectsLinks.description}
+            link={projectsLinks.link}
+            icon="ri-github-line" 
+          />
+          )
+        })}
+       
       </div>
     </section>
   );
