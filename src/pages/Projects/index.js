8572c46cc911e2b6592391410a26fa2b84d83aeb
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-31 19:10:49 -0300

    Fix: add images

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import SecondTitle from "../../components/Titles/SecondTitle";
 import ArticleProjects from "../../components/ArticleProjects"
 
-import Projects from "../../images/project-1.jpg"
+import Project1 from "../../images/project-1.png"
 
 export default () => {
   return (
@@ -14,14 +14,26 @@ export default () => {
 
       <div className="projects__container container grid">
         <ArticleProjects
-          image={Projects}
+          image={Project1}
           subtitle="Website"
           title="Restaurant Website"
           description="Project that you carry out in the design
           and structure of the layout, showing the
           design at the client's request."
           link="https://github.com/"
-          icon="ri-github-line" />
+          icon="ri-github-line" 
+        />
+        
+        <ArticleProjects
+          image={Project1}
+          subtitle="Website"
+          title="Restaurant Website"
+          description="Project that you carry out in the design
+          and structure of the layout, showing the
+          design at the client's request."
+          link="https://github.com/"
+          icon="ri-github-line" 
+        />
       </div>
     </section>
   );
