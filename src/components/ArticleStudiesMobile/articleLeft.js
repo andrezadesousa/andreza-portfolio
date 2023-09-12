da61f1b9f305e03a1f4584024ca1defed77cde5c
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-09-12 16:52:18 -0300

    Fix: create a neews components

---
+++ src/components/ArticleStudiesMobile/articleLeft.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+export const DataLeft = ({ title, subtitle, date }) => {
+  return (
+    <div>
+      <h3 className="qualification__title">{title}</h3>
+      <span className="qualification__subtitle">{subtitle}</span>
+      <div className="qualification__calendar">
+        <i class="ri-calendar-2-line"></i>
+        {date}
+      </div>
+    </div>
+  );
+};
--- src/pages/StudiesMobile/index.js
+++ src/pages/StudiesMobile/index.js
@@ -1,159 +1,110 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import "./index.css"
+import "./index.css";
 
 import SecondTitle from "../../components/Titles/SecondTitle";
 
-import { StudiesIconMobile } from "../../components/StudiesIconMobile"
+import { StudiesIconMobile } from "../../components/StudiesIconMobile";
+import { DataRight } from "../../components/ArticleStudiesMobile/articleRight";
+import { DataLeft } from "../../components/ArticleStudiesMobile/articleLeft";
 
 export default () => {
   return (
     <section className="qualification section" id="studies-mobile">
       <SecondTitle title="Qualificação." />
-      
+
       <div className="qualification__container container">
         <StudiesIconMobile firstText="Cursos" secondText="Bootcamps" />
-  
+
         <div className="qualification__section">
-
           {/*Qualification content 1 */}
-          <div className="qualification__content qualification__active" data-content id="education">
-            {/*Qualification 1 */}
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
-
+          <div
+            className="qualification__content qualification__active"
+            data-content
+            id="education"
+          >
+            {/*Qualification Left */}
+            <div className="qualification__data left">
+              <DataLeft
+                title="Gestão da Tecnologia da Informação"
+                subtitle="Faculdade de Tecnologia da Informação"
+                date="2018 - 2021"
+              />
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
             </div>
 
-            {/*Qualification 2 */}
-            <div className="qualification__data">
+            {/*Qualification Right */}
+            <div className="qualification__data right">
               <div></div>
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
-              <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
+              <DataRight
+                title="Curso de JavaScript"
+                subtitle="B7Web Cursos"
+                date="2023 - estudando"
+              />
             </div>
 
-            {/*Qualification 3 */}
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
-
+            {/*Qualification Left */}
+            <div className="qualification__data left">
+              <DataLeft
+                itle="Curso de ReactJS"
+                subtitle="B7Web Cursos"
+                date="2023 - estudando"
+              />
               <div>
                 <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
               </div>
             </div>
 
-            {/*Qualification 4 */}
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounder"></span>
-                {/*<span className="qualification__line"></span>*/}
-              </div>
-              <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
-            </div>
-            
           </div>
 
           {/*Qualification content 2 */}
           <div className="qualification__content" data-content id="work">
-            {/*Qualification 1 */}
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Frontend developer</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
-
+            {/*Qualification Left */}
+            <div className="qualification__data left">
+              <DataLeft
+                title="Gestão da Tecnologia da Informação"
+                subtitle="Faculdade de Tecnologia da Informação"
+                date="2018 - 2021"
+              />
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
             </div>
 
-            {/*Qualification 2 */}
-            <div className="qualification__data">
+            {/*Qualification Right */}
+            <div className="qualification__data right">
               <div></div>
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
+              <DataRight
+                title="Curso de JavaScript"
+                subtitle="B7Web Cursos"
+                date="2023 - estudando"
+              />
+            </div>
+
+            {/*Qualification Left */}
+            <div className="qualification__data left">
+              <DataLeft
+                itle="Curso de ReactJS"
+                subtitle="B7Web Cursos"
+                date="2023 - estudando"
+              />
               <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
+                <span className="qualification__rounder"></span>
               </div>
             </div>
 
-            {/*Qualification 3 */}
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Computer Enginner</h3>
-                <span className="qualification__subtitle">
-                  Peru - University
-                </span>
-                <div className="qualification__calendar">
-                  <i class="ri-calendar-2-line"></i>
-                  2009 - 2014
-                </div>
-              </div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-                {/*<span className="qualification__line"></span>*/}
-              </div>
-            </div> 
           </div>
         </div>
       </div>
