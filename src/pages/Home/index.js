7d627900a0702f46d27a424f9fca649ed2225288
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-09-20 21:09:06 -0300

    Fix: change home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,66 +2,59 @@
 import React from "react";
 import "./index.css";
 
-import Perfil from "../../images/teste2.jpeg";
-import Arrow from "../../images/curved-arrow.svg";
-import Line from "../../images/random-lines.svg";
-
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import WorkIcon from "@mui/icons-material/Work";
+import Perfil from "../../images/homeImage.jpeg";
 
 export default () => {
-  return (    
+  return (
     <section className="home section" id="home">
       <div className="home__container container grid">
-        <h1 className="home__name">Andreza Sousa </h1>
-
-        <div className="home__perfil">
-          <div className="home__image">
-            <img src={Perfil} className="home__img" alt="Perfil"></img>
-
-            <div className="home__shadow"></div>
-
-            <img src={Arrow} className="home__arrow" alt="Perfil"></img>
-            <img src={Line} className="home__line" alt="Perfil"></img>
-          </div>
-
-          <div className="home__social">
-            <a
-              href="https://www.linkedin.com/in/sousa-andreza/"
-              rel="noreferrer"
-              target="_blank"
-              class="home__social-link"
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              href="https://github.com/andrezadesousa"
-              rel="noreferrer"
-              target="_blank"
-              class="home__social-link"
-            >
-              <GitHubIcon />
-            </a>
-            <a
-              href="https://andreza-sousa.web.app/"
-              rel="noreferrer"
-              target="_blank"
-              class="home__social-link"
-            >
-              <WorkIcon />
-            </a>
-          </div>
+        <div className="home__img-bg">
+          <img src={Perfil} alt="Perfil" className="home__img" />
         </div>
-
-        <div className="home__info">
+        <div className="home__social">
+          <a
+            href="https://www.linkedin.com/in/sousa-andreza/"
+            rel="noreferrer"
+            target="__blank"
+            className="home__social-link"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/andrezadesousa"
+            rel="noreferrer"
+            target="__blank"
+            className="home__social-link"
+          >
+            Github
+          </a>
+          <a
+            href="/"
+            rel="noreferrer"
+            target="__blank"
+            className="home__social-link"
+          >
+            Instagram
+          </a>
+        </div>
+        <div className="home__data">
+          <h1 className="home__title">
+            Andreza <b>Sousa</b>
+          </h1>
           <p className="home__description">
-            <b>Desenvolvedora Frontend</b> e aspirante a desenvolvedora
-            FullStack. Com experiência em desenvolvimento web e mobile,
-            almejando o melhor para os projetos.
-            <br></br>“Good design is good business.”
-            <cite></cite>- Thomas J. Watson
+            Desenvolvedora Frontend à dois anos e aspirante a desenvolvedora
+            FullStack. Sou graduada em Gestão da Tecnologia da Informação e
+            apaixonada por desenvolver e criar páginas web. Atuei em diferentes
+            projetos, web e mobile, sendo nacionais e internacionais. Tendo a
+            oportunidade de trabalhar e conhecer diferentes tecnologias.
           </p>
+          <span className="home__phrase">“Good design is good business.”</span>
+          <cite></cite>- Thomas J. Watson
+          <div className="home__btns">
+            <a href="/" className="buttons button--gray button--small">
+              Contato
+            </a>
+          </div>
         </div>
       </div>
     </section>
