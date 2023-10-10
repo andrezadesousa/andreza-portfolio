53d3a515b039b345d3642f428b4325e8d5deadc3
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-10-10 16:22:33 -0300

    Fix: add firebase confg

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Header from "./pages/Header";
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
 import { Skills } from "./pages/Skills";
-import { Studies } from "./pages/Studies";
-import StudiesMobile from "./pages/StudiesMobile";
+import Studies from "./pages/Studies";
 import Projects from "./pages/Projects";
 import Footer from "./pages/Footer";
 
@@ -21,7 +20,7 @@ function App() {
           <Home />
           <Experience />
           <Skills />
-          <StudiesMobile />
+          <Studies />
           <Projects />
           <Footer />
         </main>
