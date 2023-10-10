abee0d9512e8c524870b3efa6f6a3b6dc7c71c96
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-10-10 16:44:38 -0300

    Fix: refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./index.css"
+
+import { Pages } from './pages/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pages />
   </React.StrictMode>
 );
 
---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./index.css";
+
+import Header from "./Header";
+import Home from "./Home";
+import Experience from "./Experience";
+import Studies from "./Studies";
+import Projects from "./Projects";
+import Footer from "./Footer";
+
+import { Skills } from "./Skills";
+
+export const Pages = () => {
+  return (
+    <React.StrictMode>
+      <body>
+        <Header />
+        <main className="main">
+          <Home />
+          <Experience />
+          <Skills />
+          <Studies />
+          <Projects />
+          <Footer />
+        </main>
+      </body>
+    </React.StrictMode>
+  );
+};
