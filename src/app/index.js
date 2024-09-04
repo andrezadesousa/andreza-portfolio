1d46e2879deecf408d2cbe201fff805db4fbb3b7
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-09-04 14:03:11 -0300

    creating about section

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Header } from "./components/header";
 import "./global/style.css";
-import { Home } from "./pages/home";
+import { About } from "./pages/about";
 
 export const Pages = () => {
   return (
     <body>
       <Header />
       <main className="main">
-        <Home />
+        <About />
       </main>
     </body>
   );
--- src/app/pages/about/index.js
+++ src/app/pages/about/index.js
@@ -1,30 +1,31 @@
 import React from "react";
 import "./style.css";
+
 import { TitleSection } from "../../components/titleSection";
 
-export const Home = () => {
+export const About = () => {
   return (
-    <section className="home section " id="home">
-      <div className="home__container grid">
+    <section className="about section " id="about">
+      <div className="about__container grid">
         <img
-          className="home__image"
+          className="about__image"
           src="https://media.licdn.com/dms/image/v2/D4D03AQE3rLLjfQ4YBw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724525257776?e=1729728000&v=beta&t=kniZ3j3Y_2c9OSa8RVGFzvAdoOopJ3r5P9csz_Sc3EQ"
         />
-        <div className="home__content">
+        <div className="about__content">
           <TitleSection title="Sobre mim" />
-          <p className="home__text">
+          <p className="about__text">
             Olá, me chamo <strong>Andreza Sousa</strong> e atuo como
             desenvolvedora Front-end há dois anos.
           </p>
-          <p className="home__text">
+          <p className="about__text">
             Sou graduada em Gestão da Tecnologia da Informação e apaixonada por
             desenvolver e criar páginas web. Atuei em diferentes projetos, web e
             mobile, sendo eles nacionais e internacionais. Tendo a oportunidade
             de trabalhar e conhecer diferentes tecnologias.
           </p>
-          <div className="home__phrase">
+          <div className="about__phrase">
             <p>“Good design is good business.”</p>
-            <p className="home__phrase-name">- Thomas J. Watson</p>
+            <p className="about__phrase-name">- Thomas J. Watson</p>
           </div>
         </div>
       </div>
